37672ffe51ea1d103459ade68bf9a27cfa07bed9
Datyak <isaaclovy@gmail.com>
2026-08-24 18:09:18 -0400

    Pickup and drop a target

--- scripts/pickup_target.gd
+++ scripts/pickup_target.gd
@@ -1,23 +1,25 @@
-extends Node3D
+class_name PickupTarget extends Node3D
 
 var hover_target:bool = false
 var dragging:bool= false
-
-signal picked_up
+var follow_target:Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _hover() -> void:
+	print("Is target")
 	hover_target = true
 
 func _hover_end() -> void:
+	print("Is no longer target")
 	hover_target = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == 1:
-		if event.is_pressed() and hover_target:
-			dragging = true
-			picked_up.emit()
+func interact(cursor: Cursor):
+	if hover_target:
+		cursor.pick_up(self)
+
+func _process(_delta: float) -> void:
+	if dragging and follow_target:
+		global_position = follow_target.global_position
