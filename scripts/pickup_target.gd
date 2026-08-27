f750eeed5b582fd68644ab259e2ed2540951b2df
Datyak <isaaclovy@gmail.com>
2026-08-27 11:25:07 -0400

    Shadow & Pickup area movement fix

--- scripts/pickup_target.gd
+++ scripts/pickup_target.gd
@@ -12,6 +12,7 @@ class_name PickupTarget extends Node3D
 @onready var parent:Node = self.get_parent().get_parent().get_parent()
 @onready var ui:UnitUI = $"../../CharacterStats/UnitInspectUi"
 @onready var indicator:Node3D = $Indicator
+@onready var mesh:Node3D = $Mesh
 
 var hover_target:bool = false
 var dragging:bool= false
@@ -27,14 +28,22 @@ var first_position:Vector3
 ## Where the entity was last, and where to return on a failed drop.
 var last_position:Vector3
 
+var collision_height:int = 0
+
+var area3d:Area3D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ui.visible = false
 	if is_moved_as_indicator:
 		dragged_element = indicator
+		area3d = $Indicator_Area
 	else:
-		dragged_element =  get_parent_node_3d()
+		dragged_element =  mesh
+		area3d = $Area
 		indicator.visible = false
+		
+	
 
 func set_initial_position() -> void:
 	first_position = dragged_element.global_position
@@ -53,15 +62,11 @@ func interact(cursor: Cursor, interact_control:Cursor.CursorTarget) -> void:
 	if hover_target:
 		if interact_type == interact_control:
 				last_position = global_position
+				collision_height = area3d.global_position.y
 				cursor.pick_up(self)
 				on_pick_up(interact_type)
 
 func on_pick_up(interact_control:Cursor.CursorTarget)->void:
-	var area3d:Area3D
-	if is_moved_as_indicator:
-		area3d = indicator.get_node("Area3D")
-	else:
-		area3d = $Area3D
 	for area in area3d.get_overlapping_areas():
 		if area.is_in_group("Dropoff"):
 			var dropoff = area.get_parent() as Dropoff
@@ -71,11 +76,6 @@ func on_pick_up(interact_control:Cursor.CursorTarget)->void:
 
 func on_place(interact_control:Cursor.CursorTarget) -> void:
 	var dropped:bool = false
-	var area3d:Area3D
-	if is_moved_as_indicator:
-		area3d = indicator.get_node("Area3D")
-	else:
-		area3d = $Area3D
 	for area in area3d.get_overlapping_areas():
 		if area.is_in_group("Dropoff"):
 			var dropoff = area.get_parent() as Dropoff
@@ -86,12 +86,17 @@ func on_place(interact_control:Cursor.CursorTarget) -> void:
 		# No drop off point found...
 		var tween = get_tree().create_tween()
 		tween.tween_property(dragged_element, "global_position", last_position, 0.6)
-
+		area3d.global_position = last_position
+	
 func _process(_delta: float) -> void:
 	if dragging and follow_target:
-		dragged_element.global_position = follow_target.global_position
+		var target_pos:Vector3 = follow_target.global_position
+		dragged_element.global_position = target_pos
+		var area_pos = Vector3(target_pos.x, collision_height, target_pos.z)
+		area3d.global_position = area_pos
 
 func reset_position() ->void:
 	on_pick_up(interact_type)
 	var tween = get_tree().create_tween()
 	tween.tween_property(dragged_element, "global_position", first_position, 0.6)
+	area3d.global_position = first_position
