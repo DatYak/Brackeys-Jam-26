7c6e8cdfdc6c2a91048a8129e32d1c385b987e9c
Datyak <isaaclovy@gmail.com>
2026-08-28 15:27:13 -0400

    mission banner snapping

--- scripts/dropoff_mission.gd
+++ scripts/dropoff_mission.gd
@@ -15,6 +15,7 @@ func _drop_entity(entity:PickupTarget) -> bool:
 		return false
 	
 	assigned_general = entity.parent as General
+	entity.dragged_element.global_position = global_position + Vector3.UP * 2.0
 	assigned_general.troop_added_to_party.connect(ui.display)
 	return true
 
