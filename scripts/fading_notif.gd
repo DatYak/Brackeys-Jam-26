82f65824d0e021bf834a570eb35b8b2a4749d343
Datyak <isaaclovy@gmail.com>
2026-08-28 07:47:14 -0400

    Improved timing

--- scripts/fading_notif.gd
+++ scripts/fading_notif.gd
@@ -7,8 +7,11 @@ func setup(text:String, duration:float) -> void:
 	global_position = Vector2(0, - (get_viewport_rect().size.y/2) - 100)
 	var end_pos = Vector2(0, (get_viewport_rect().size.y/2) + 100)
 	label.text = text
+	
+	# Account for duration in time
+	var disply_duration = duration - (transition_duration * 2)
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "global_position", Vector2(0,0), transition_duration)
-	tween.tween_interval(duration)
+	tween.tween_interval(disply_duration)
 	tween.tween_property(self, "global_position", end_pos, transition_duration)
 	tween.tween_callback(queue_free)
