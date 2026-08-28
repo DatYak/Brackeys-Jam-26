7d0853963b46d79cc1cc629b152b8d4e6a69adad
Datyak <isaaclovy@gmail.com>
2026-08-28 12:42:46 -0400

    Mission difficulty labels

--- scripts/mission_inspect_ui.gd
+++ scripts/mission_inspect_ui.gd
@@ -1,5 +1,9 @@
 class_name MissionUI extends Control
 
+const EASY_LABEL = "(E) Task of "
+const MID_LABEL = "(M) Test of "
+const HARD_LABEL = "(H) Trial of "
+
 @onready var mission:Mission = $".."
 
 @onready var name_lbl : Label = $Background/Name/Label
@@ -30,3 +34,26 @@ func display():
 		lose_lbl.text = "-Sanity"
 		lose_lbl.label_settings.font_color = Color("4477aa")
 	
+	var diff_label:String = ""
+	
+	var difficulty:float = mission.difficulty_percent()
+	if difficulty < 0.33:
+		diff_label = EASY_LABEL
+	else: if difficulty < 0.66:
+		diff_label = MID_LABEL
+	else:
+		diff_label = HARD_LABEL
+	
+	if mission.skillRequired == CharacterStats.SkillType.MIGHT:
+		difficulty_lbl.label_settings.font_color = Color("#d95f02")
+		diff_label += "Might"
+	
+	if mission.skillRequired == CharacterStats.SkillType.GUILE:
+		difficulty_lbl.label_settings.font_color = Color("#4daf4a")
+		diff_label += "Guille"
+		
+	if mission.skillRequired == CharacterStats.SkillType.NERVE:
+		difficulty_lbl.label_settings.font_color = Color("#3a8eba")
+		diff_label += "Nerve"
+		
+	difficulty_lbl.text = diff_label
